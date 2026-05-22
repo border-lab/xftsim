e72a75b37439a67e1853a464d90db9f9c2e5c8e9
Richard Border <border.richard@gmail.com>
2026-05-21 21:35:58 -0400

    setup.py: read version dynamically from __init__.py; fix repo URL

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
+import re
+from pathlib import Path
+
 from setuptools import setup, find_packages
+
+
+def read_version():
+    """Read __version__ from xftsim/__init__.py without importing the package."""
+    init = Path(__file__).parent / "xftsim" / "__init__.py"
+    match = re.search(
+        r'__version__\s*=\s*["\']([^"\']+)["\']', init.read_text()
+    )
+    if not match:
+        raise RuntimeError("Unable to find __version__ in xftsim/__init__.py")
+    return match.group(1)
+
 
 setup(
     name='xftsim',
 
-    version='0.9a.dev109',
+    version=read_version(),
 
     description='Forward-time genetics simulator',
     long_description=open('README.md').read(),
@@ -12,7 +27,7 @@ setup(
     author="Richard Border",
     author_email="border.richard@gmail.com",
 
-    url='https://github.com/rborder/xftsim',
+    url='https://github.com/border-lab/xftsim',
 
     packages=find_packages(include=[
         'xftsim', 'xftsim*',
